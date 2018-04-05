44da0223077bcbc19abaec3a84d3c8d36130a8dd
mkr <marcelo.krebber@twt-dh.de>
2018-04-05 19:49:04 +0100

    # sidebar more improvements

--- test-05/src/components/active-contact.js
+++ test-05/src/components/active-contact.js
@@ -2,15 +2,30 @@ import React, {Component} from 'react';
 
 export default class ActiveContact extends Component {
 
+    renderTextArea(contact) {
+
+        if(this.props.chat) {
+            return(
+                <div className="collapse" id={`chat${contact.user}`}>
+                    <div className='active-chat'>
+                        <textarea placeholder="You.."/>
+                    </div>
+                </div>
+            );
+        }
+    }
+
     render() {
         const contact = this.props.contact;
-        console.log('active-contact', contact);
 
         return (
             <div className='active-contact'>
                 <a href="#"><img src={contact.thumb}/>{contact.user}</a>
-                {this.props.chat ? <a href=''><i className="fa fa-commenting-o" aria-hidden="true"/></a> : ''}
+                {this.props.chat ? <a data-toggle="collapse" href={`#chat${contact.user}`}><i className="fa fa-commenting-o" aria-hidden="true"/>3</a> : ''}
                 <span>{contact.when}</span>
+
+                {this.renderTextArea(contact)}
+
             </div>
         );
     }
