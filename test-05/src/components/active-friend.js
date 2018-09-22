da46b178c1e21e1f7ac7e7d67c6b1483574e0e71
mkr <marcelo.krebber@twt-dh.de>
2018-09-22 19:43:12 +0100

    message count badge.. alpha code

--- test-05/src/components/active-friend.js
+++ test-05/src/components/active-friend.js
@@ -41,7 +41,7 @@ class ActiveChat extends Component {
     render() {
         const {chatId, user, chat, callback} = this.props;
 
-        return <div className="d-inline">
+        return <div className="active-frame">
             <button title={`Chat with ${user.firstname}`} className="btn btn-billboard btn-sm"
                     onClick={(event) => {
                         if (event === null) return;
@@ -53,7 +53,7 @@ class ActiveChat extends Component {
                             toggle.classList.toggle('active-show');
                             callback && callback({isOpen: toggle.classList.contains('active-show')});
                             setTimeout(() => {
-                               document.getElementById(`textarea-${user.username}`).focus();
+                                document.getElementById(`textarea-${user.username}`).focus();
                             }, 500);
 
                         }
@@ -115,6 +115,8 @@ class ActiveFriend extends Component {
             this.props.asyncFetchChatEntries(user.username, chatId, () => {
                 this.localstate.set({isLoaded: true})
             });
+        } else {
+            this.forceUpdate();
         }
     }
 
@@ -159,6 +161,10 @@ class ActiveFriend extends Component {
                                     d="M12,0A12,12 0 0,1 24,12A12,12 0 0,1 12,24A12,12 0 0,1 0,12A12,12 0 0,1 12,0M12,2A10,10 0 0,0 2,12C2,14.4 2.85,16.6 4.26,18.33L18.33,4.26C16.6,2.85 14.4,2 12,2M12,22A10,10 0 0,0 22,12C22,9.6 21.15,7.4 19.74,5.67L5.67,19.74C7.4,21.15 9.6,22 12,22Z"/>
                             </svg>
                         </span>}
+                        {!this.localstate.get().isOpen && chatId && <span className="counter-thumb">
+                            <div className="badge badge-light d-inline">{chat.length}</div>
+                        </span>}
+
                     </div>
                     {fullname}
                 </Link>
