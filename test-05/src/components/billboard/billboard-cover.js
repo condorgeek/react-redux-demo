a8b603f28636ff1018ae511db1029b8e1b2da3e8
mkr <marcelo.krebber@twt-dh.de>
2018-11-22 09:55:24 +0000

    some cosmetics..

--- test-05/src/components/billboard/billboard-cover.js
+++ test-05/src/components/billboard/billboard-cover.js
@@ -168,7 +168,7 @@ class BillboardCover extends Component {
         return <div className="generic-cover-tooltip">
             <img className={isBlocked ? 'blocked-img': ''} src={avatar}/><span className="d-block">{text}</span> {user.firstname} {user.lastname}
             {isBlocked &&
-            <svg viewBox="0 0 24 24">
+            <svg viewBox="-0.2 -0.2 24.8 24.8">
                 <path
                     d="M12,0A12,12 0 0,1 24,12A12,12 0 0,1 12,24A12,12 0 0,1 0,12A12,12 0 0,1 12,0M12,2A10,10 0 0,0 2,12C2,14.4 2.85,16.6 4.26,18.33L18.33,4.26C16.6,2.85 14.4,2 12,2M12,22A10,10 0 0,0 22,12C22,9.6 21.15,7.4 19.74,5.67L5.67,19.74C7.4,21.15 9.6,22 12,22Z"/>
             </svg>}
