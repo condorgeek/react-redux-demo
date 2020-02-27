3464188e04a5e2a330c154bc4e956204ab14a394
Marcelo Krebber <marcelo.krebber@gmail.com>
2020-02-27 16:48:40 +0000

    refactored join/leave buttons on generic cover

--- test-05/src/components/billboard/billboard-generic-cover.js
+++ test-05/src/components/billboard/billboard-generic-cover.js
@@ -13,13 +13,11 @@
 
 import holderjs from 'holderjs';
 import moment from 'moment';
-import {bindRawTooltip, bindTooltip, showTooltip} from "../../actions/tippy-config";
 import toastr from "../../../node_modules/toastr/toastr";
 
-import React, {Component} from 'react';
-import ReactDOMServer from 'react-dom/server';
+import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
-import {Link, withRouter} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 import {
     asyncUpdateUserAvatar, asyncValidateAuth, asyncAddFollowee, asyncAddFriend, loginStatus
@@ -27,20 +25,21 @@ import {
 import {asyncJoinSpace, asyncLeaveSpace, updateGenericData, updateCreateSpace, updateDeleteSpace,
     asyncUpdateSpaceCover, asyncFetchGenericData} from "../../actions/spaces";
 
-import {EVENT_SPACE, ACTION_LEAVE_SPACE, ACTION_JOIN_SPACE} from "../../actions/spaces";
+import {EVENT_SPACE} from "../../actions/spaces";
 import CoverUploadModal from "./dialogs/cover-upload-modal";
 import CoverSlider from "../slider/cover-slider";
 import HeadlineUserEntry from "../headlines/headline-user-entry";
 import {getStaticImageUrl} from "../../actions/environment";
 import {
     BiggerIcon, FlatButton,
-    FlatButtonBounded,
     FlatIcon,
     Icon,
     NavigationGroup,
     NavigationRow
 } from "../navigation-buttons/nav-buttons";
 import {ConfigurationContext} from "../configuration/configuration";
+import {getAuthorizedUsername, isAuthorized, isSuperUser} from "../../selectors";
+import {Spinner} from "../util/spinner";
 
 class Coverholder extends Component {
     render() {
@@ -57,76 +56,21 @@ class BillboardGenericCover extends Component {
         this.props.asyncFetchGenericData(props.authorization.user.username, props.spacepath);
 
         this.localstate = this.localstate.bind(this)({location: props.location});
-        this.handleTooltipAction = this.handleTooltipAction.bind(this);
     }
 
     localstate(data) {
         let state = data;
-        let tooltips = [];
+        // let tooltips = [];
         return {
             setState(newstate) { state = {...state, ...newstate}; return state; },
             getState() { return state; },
-            pushTooltip(tooltip) { tooltips.push(tooltip)},
-            removeTooltips() {
-                tooltips.forEach(tooltip => {tooltip.destroy();}); tooltips = [];
-            }
+            // pushTooltip(tooltip) { tooltips.push(tooltip)},
+            // removeTooltips() {
+            //     tooltips.forEach(tooltip => {tooltip.destroy();}); tooltips = [];
+            // }
         }
     }
 
-    componentWillUnmount() {
-        this.localstate.removeTooltips();
-    }
-
-    renderMembersTooltip(authorization, genericdata) {
-        const {user} = genericdata.space;
-        const avatar = getStaticImageUrl(user.avatar);
-
-        const isOwner = genericdata && (genericdata.space.user.username === authorization.user.username);
-        const isMember = genericdata && genericdata.isMember;
-
-        const data = {authorization: authorization, genericdata: genericdata, username: user.username,
-            spaceId: genericdata ? genericdata.space.id : null, memberId: isMember ? genericdata.member.id : null};
-
-        return <div className="generic-cover-tooltip">
-            <img src={avatar}/>{` Space created by ${user.firstname}`}
-
-            {!isOwner && !isMember &&
-            <button className="btn btn-tooltip btn-sm" data-props={JSON.stringify({...data, action: ACTION_JOIN_SPACE})}>
-                <span><i className="fas fa-user-plus"/></span>Join Space
-            </button>}
-
-            {!isOwner && isMember &&
-            <button className="btn btn-tooltip btn-sm" data-props={JSON.stringify({...data, action: ACTION_LEAVE_SPACE})}>
-                <span><i className="fas fa-user-minus"/></span>Leave Space
-            </button>}
-        </div>
-    }
-
-    handleTooltipAction(event, data, timestamp) {
-        if (data === undefined || timestamp === undefined) return;
-        const props = JSON.parse(data);
-        const {authorization, genericdata, username, spaceId, memberId} = props;
-
-        switch (props.action) {
-            case ACTION_JOIN_SPACE:
-                this.props.asyncJoinSpace(authorization.user.username, spaceId, member => {
-                    this.props.updateCreateSpace(genericdata.space);
-                    toastr.info(`You have joined ${genericdata.space.name}`);
-                });
-
-                return;
-
-            case ACTION_LEAVE_SPACE:
-                memberId && this.props.asyncLeaveSpace(authorization.user.username, spaceId, memberId, member => {
-                        this.props.updateDeleteSpace(genericdata.space);
-                        toastr.info(`You have left ${genericdata.space.name}`);
-                    });
-                return;
-
-            default:
-                return;
-        }
-    }
 
     getStartDate(genericdata) {
         if (!genericdata) return "";
@@ -143,9 +87,14 @@ class BillboardGenericCover extends Component {
     }
 
     renderCoverBanner(genericdata) {
-        if(!genericdata) return (<div className="fa-2x billboard-spinner">
-            <i className="fas fa-spinner fa-spin"/>
-        </div>);
+
+        // TODO replace with Spinner
+        // if(!genericdata) return (<div className="fa-2x billboard-spinner">
+        //     <i className="fas fa-spinner fa-spin"/>
+        // </div>);
+
+        if(!genericdata) return <Spinner/>;
+
         const {space} = genericdata;
 
         if(!space.media) return <Coverholder text={space.name} ref={() => holderjs.run()}/>;
@@ -167,7 +116,6 @@ class BillboardGenericCover extends Component {
             const target = `/${child.user.username}/space/${child.id}`;
 
             return <li className="nav-item">
-                {/*<a className="nav-link" href="#">{child.name}</a>*/}
                 <Link className="nav-link" to={target} href="#">{child.name}</Link>
             </li>
         });
@@ -179,38 +127,92 @@ class BillboardGenericCover extends Component {
 
 
     resolveUserName(authorization, genericdata) {
-        if(!genericdata) return authorization.user.username;
+        const {authname, isSuperUser} = this.props;
+        if(!genericdata) return authname;
 
         const isOwner = genericdata.isMember && genericdata.member.role === 'OWNER';
-        const isSuperUser = authorization && authorization.user.isSuperUser;
-
-        return isSuperUser && !isOwner ? genericdata.space.user.username : authorization.user.username;
+        return isSuperUser && !isOwner ? genericdata.space.user.username : authname;
     }
+
+    joinSpace = (event) => {
+        event.preventDefault();
+        const {authname, spaceId, genericdata} = this.props;
+
+        this.props.asyncJoinSpace(authname, spaceId, member => {
+            this.props.updateCreateSpace(genericdata.space);
+            toastr.info(`You have joined ${genericdata.space.name}`);
+        });
+    };
+
+    leaveSpace = (event) => {
+        event.preventDefault();
+        const {authname, spaceId, genericdata} = this.props;
+        const isMember = genericdata && genericdata.isMember;
+        const memberId = isMember ? genericdata.member.id : null;
+
+        memberId && this.props.asyncLeaveSpace(authname, spaceId, memberId, member => {
+            this.props.updateDeleteSpace(genericdata.space);
+            toastr.info(`You have left ${genericdata.space.name}`);
+        });
+    };
+
+    renderJoinButtons = (inContext) => {
+        const {authname, genericdata} = this.props;
+        const isMember = genericdata && genericdata.isMember;
+        const isOwner = genericdata && (genericdata.space.user.username === authname);
+
+        if(!inContext) return null;
+
+        return <Fragment>
+            <FlatButton btn small title='Space members'
+                        className='btn-outline-light mobile-headline-button'
+                        onClick={(event) => {
+                            console.log('FRIENDS', event);
+                        }}>
+                <Icon className="fas fa-user-friends mr-1"/>
+                <span className='mobile-headline-text'>
+                                        {genericdata.members} Members
+                                    </span>
+            </FlatButton>
+
+            {!isOwner && !isMember && <FlatButton btn small title='Join space'
+                        className='btn-outline-light mobile-headline-button'
+                        onClick={this.joinSpace}>
+                <Icon className='fas fa-user-plus mr-1'/>
+                <span className='mobile-headline-text'>Join</span>
+            </FlatButton>}
+
+            {!isOwner && isMember && <FlatButton btn small title='Leave space'
+                        className='btn-outline-light mobile-headline-button'
+                        onClick={this.leaveSpace}>
+                <Icon className='fas fa-user-minus mr-1'/>
+                <span className='mobile-headline-text'>Leave</span>
+            </FlatButton>}
+        </Fragment>
+    };
+
 
     render() {
         const {location} = this.localstate.getState();
-        const {authorization, genericdata, ownername, spacepath, spaceId, Lang} = this.props;
+        const {authorization, genericdata, isAuthorized, isSuperUser, ownername, spacepath, spaceId, Lang} = this.props;
 
         if(location.pathname !== this.props.location.pathname) {
-            this.localstate.removeTooltips();
+            // this.localstate.removeTooltips();
             this.localstate.setState({location: this.props.location});
             this.props.asyncFetchGenericData(authorization.user.username, spacepath);
-            return "";
+            return null;
         }
 
         const inContext = genericdata && (genericdata.space.id.toString() === spaceId);
         const isMember = genericdata && genericdata.isMember;
         const isMembersOnly = genericdata && genericdata.space.access === 'RESTRICTED';
         const isEvent = genericdata && genericdata.space.type === 'EVENT';
-        const isAuthorized = authorization.status === loginStatus.SUCCESS;
-        const isSuperUser = authorization && authorization.user.isSuperUser;
         const spacedata = genericdata && genericdata.spacedata;
         const startDate = this.getStartDate(genericdata);
 
-        inContext && this.localstate.removeTooltips();
+        // inContext && this.localstate.removeTooltips();
 
-        return (
-            <div className='billboard-cover'>
+        return <div className='billboard-cover'>
                 <span title={this.getTitle(genericdata, startDate)}>
                     {this.renderCoverBanner(genericdata)}
                 </span>
@@ -228,23 +230,10 @@ class BillboardGenericCover extends Component {
                                 <Icon className="fas fa-mask mr-1"/>
                             </FlatButton>}
 
-                            {inContext && <FlatButtonBounded btn small title='Space members'
-                                               className='btn-outline-light mobile-headline-button'
-                                               onBound={(elem) => {
-                                                   if (elem === null || genericdata === undefined) return;
-                                                   const html = ReactDOMServer.renderToStaticMarkup(this.renderMembersTooltip(authorization, genericdata));
-                                                   const tooltip = bindTooltip(elem, html, {callback: this.handleTooltipAction});
-                                                   this.localstate.pushTooltip(tooltip);
-                                               }}
-                                               onClick={(e) => console.log('FRIEND')}>
-                                <Icon className="fas fa-user-friends mr-1"/>
-                                <span className='mobile-headline-text'>
-                                    {genericdata.members} Members
-                                </span>
-                            </FlatButtonBounded>}
+                            {this.renderJoinButtons(inContext)}
 
-
-                            {isAuthorized && (isMember || isSuperUser) && <FlatIcon circle btn primary title='Upload cover image' className='mobile-headline-icon' onClick={(e) => {
+                            {isAuthorized && (isMember || isSuperUser) &&
+                            <FlatIcon circle btn primary title='Upload cover image' className='mobile-headline-icon' onClick={(e) => {
                                 e.preventDefault();
                                 this.uploadModalRef.onOpen();
                             }}>
@@ -280,13 +269,15 @@ class BillboardGenericCover extends Component {
                 {genericdata && this.renderSubMenu(genericdata.space)}
 
             </div>
-        );
     }
 }
 
 
 function mapStateToProps(state) {
     return {authorization: state.authorization,
+        authname: getAuthorizedUsername(state),
+        isAuthorized: isAuthorized(state),
+        isSuperUser: isSuperUser(state),
         genericdata: state.genericdata ? state.genericdata.payload : state.genericdata};
 }
 
